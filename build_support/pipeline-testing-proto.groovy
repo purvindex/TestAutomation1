8fe2798cc59fa40b206ee65a48af9ca22a2ec5b8
Eyituoyo Ekperigin <tekperigin@index-analytics.com>
2024-03-15 09:34:59 -0400

    testing fixes

--- build_support/pipeline-testing-proto.groovy
+++ build_support/pipeline-testing-proto.groovy
@@ -47,10 +47,16 @@ pipeline {
           # Install required software and modules
           apt-get update -y && \
             apt-get install -y \
-              libglib2.0-0=2.50.3-2 \
-              libnss3=2:3.26.2-1.1+deb9u1 \
-              libgconf-2-4=3.2.6-4+b1 \
-              libfontconfig1=2.11.0-6.7+b1
+              libglib2.0-0 \
+              libnss3 \
+              libgconf-2-4 \
+              libfontconfig1
+
+#            apt-get install -y \
+#              libglib2.0-0=2.50.3-2 \
+#              libnss3=2:3.26.2-1.1+deb9u1 \
+#              libgconf-2-4=3.2.6-4+b1 \
+#              libfontconfig1=2.11.0-6.7+b1
 
           # Check Python version
           python --version
