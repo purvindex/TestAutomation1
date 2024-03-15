12be9ff4373b7fb313794ce237a8cd2c3ca235b6
Eyituoyo Ekperigin <tekperigin@index-analytics.com>
2024-03-15 09:29:09 -0400

    testing fixes

--- build_support/pipeline-testing-proto.groovy
+++ build_support/pipeline-testing-proto.groovy
@@ -44,10 +44,18 @@ pipeline {
         container('python') {
           sh '''
 
+          # Install required software and modules
+          apt-get update -y && \
+            apt-get install -y \
+              libglib2.0-0=2.50.3-2 \
+              libnss3=2:3.26.2-1.1+deb9u1 \
+              libgconf-2-4=3.2.6-4+b1 \
+              libfontconfig1=2.11.0-6.7+b1
+
           # Check Python version
           python --version
 
-          # Install required libraries
+          # Install required Python libraries
           python -m ensurepip --upgrade
           pip install pytest-cov pipreqs pip-tools
 
