6cfe40dc039038af1e61cb481f34566545e32259
Purvi <pdholakia@index-analytics.com>
2024-04-29 23:08:24 -0400

    configuration For remote webdriver

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,75 +1,78 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import wait
 
 from utilities import ReadConfigurations
-from selenium import webdriver
-import pytest
 
-#@pytest.fixture() this code was working
-#def setup():
- #   driver=webdriver.Chrome()
-  #  return driver
 
-############Commenting out the following code which is running good so far 28th Feb 2024 #####################
-## When you want to run Cross Browser Testing  please comment out below code ##########
-@pytest.fixture(scope="class")
+@pytest.fixture(autouse=True)
 def setup_and_teardown(request):
-    browser = ReadConfigurations.read_configuration("basic info", "browser")
-    if browser.__eq__("chrome"):
-        #driver = webdriver.Chrome()  this is working fine the single line
-        c = webdriver.ChromeOptions()
-        c.add_argument("--incognito")
-        driver = webdriver.Chrome(c)
-    elif browser.__eq__("firefox"):
-        driver = webdriver.Firefox()
-    else:
-        driver = webdriver.Edge()
-    driver.maximize_window()
-    driver.implicitly_wait(20)
-    app_url = ReadConfigurations.read_configuration("basic info", "url")
-    driver.get(app_url)
-    # uname=ReadConfigurations.read_configuration("basic info", "username")
-    # driver.find_element(By.ID, "cms-login-userId").send_keys(uname)
-    # pswd=ReadConfigurations.read_configuration("basic info", "password")
-    # driver.find_element(By.ID, "cms-login-password").send_keys(pswd)
-    request.cls.driver = driver # whichever class will request this driver is available to that class
-    yield
-    driver.quit()
+    #browser = ReadConfigurations.read_configuration("basic info", "browser")
 
-##################  above code is working fine #######################################
+    # if browser == "chrome":
+    #     #driver = webdriver.Chrome()  this is working fine the single line
+    #     # c = webdriver.ChromeOptions()
+    #     # c.add_argument("--incognito")
+    #     driver = webdriver.Chrome()
+    # elif browser == "firefox":
+    #     driver = webdriver.Firefox()
+    # elif browser == "edge":
+    #     driver = webdriver.Edge()
+    # # app_url = ReadConfigurations.read_configuration("basic info", "url")
+    # # driver.get(app_url)
+    # driver.maximize_window()
+    # driver.get("https://portalval.cms.gov/portal")
+    # driver.implicitly_wait(20)
+    # # uname=ReadConfigurations.read_configuration("basic info", "username")
+    # # driver.find_element(By.ID, "cms-login-userId").send_keys(uname)
+    # # pswd=ReadConfigurations.read_configuration("basic info", "password")
+    # # driver.find_element(By.ID, "cms-login-password").send_keys(pswd)
+    # request.cls.driver = driver # whichever class will request this driver is available to that class
+    # yield
+    # driver.quit()
+
+    #  pytest -s testCases --name chrome
+    E34_URL = "https://selenium.cloud.cms.gov"
+    x=request.config.getoption("--browser")
+    print(x)
+    if x.__eq__('chrome'):
+        options = ChromeOptions()
+        options.browser_version = '123'
+        options.platform_name = 'Windows 11'
+        options.enable_downloads = True
+        options.page_load_strategy = 'normal'
+        options.acceptInsecureCerts = True
+        options.unhandledPromptBehavior = 'accept'
+
+        #options.timeouts = 30000
+        cloud_options = {}
+        cloud_options['build'] = "build_1"
+        cloud_options['name'] = "test_abc"
+        cloud_options['authorization'] = "bHR0NmI3YjY4MGItMzBkNi00ZGJhLWI0ZWQtMzU5NDNlZDE3Zjlm"
+        options.set_capability('cloud:options', cloud_options)
+        driver = webdriver.Remote(command_executor=E34_URL + "/wd/hub",keep_alive = True,file_detector= None, options=options)
 
 
-################ Below Fixture is to run Cross Browser Testing #####################
-# @pytest.fixture(autouse=True)
-# def setup(request,browser):
-#     #browser = ReadConfigurations.read_configuration("basic info", "browser")
-#
-#     if browser == "chrome":
-#         #driver = webdriver.Chrome()  this is working fine the single line
-#         # c = webdriver.ChromeOptions()
-#         # c.add_argument("--incognito")
-#         driver = webdriver.Chrome()
-#     elif browser == "firefox":
-#         driver = webdriver.Firefox()
-#     elif browser == "edge":
-#         driver = webdriver.Edge()
-#     # app_url = ReadConfigurations.read_configuration("basic info", "url")
-#     # driver.get(app_url)
-#     driver.maximize_window()
-#     driver.get("https://portalval.cms.gov/portal")
-#     driver.implicitly_wait(20)
-#     # uname=ReadConfigurations.read_configuration("basic info", "username")
-#     # driver.find_element(By.ID, "cms-login-userId").send_keys(uname)
-#     # pswd=ReadConfigurations.read_configuration("basic info", "password")
-#     # driver.find_element(By.ID, "cms-login-password").send_keys(pswd)
-#     request.cls.driver = driver # whichever class will request this driver is available to that class
-#     yield
-#     driver.quit()
-#
-# def pytest_addoption(parser):
-#     parser.addoption("--browser")
-#     #parser.addoption("--url")
-#
+        app_url = ReadConfigurations.read_configuration("basic info", "url")
+        driver.get(app_url)
+
+        uname = ReadConfigurations.read_configuration("basic info", "username")
+        driver.find_element(By.ID, "cms-login-userId").send_keys(uname)
+
+        pswd = ReadConfigurations.read_configuration("basic info", "password")
+        driver.find_element(By.ID, "cms-login-password").send_keys(pswd)
+
+        request.cls.driver = driver  # whichever class will request this driver is available to that class
+        # request.cls.wait =wait
+        yield
+        driver.quit()
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome")
+    #parser.addoption("--url")
+    #parser.addoption("--name", action="store")
+
 # @pytest.fixture(scope="class", autouse=True)
 # def browser(request):
 #     return request.config.getoption("--browser")
@@ -78,27 +81,13 @@ def setup_and_teardown(request):
 # def url(request):
 #     return request.config.getoption("--url")
 
-#########################Cross Browser testing ends here ##########################################3
 
-############ Below is working fine for Data Driven Testing script ###################
-@pytest.fixture(scope="class")
-def setupandteardown(request):
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    driver.implicitly_wait(5)
-    driver.get("https://portalval.cms.gov/portal/")
-    #driver.get("https://portaldev.cms.gov/portal/")
-    request.cls.driver = driver
-    yield
-    driver.quit()
-#########################################################################
+# @pytest.fixture(scope="class", autouse=True)
+# def name(pytestconfig):
+#     return pytestconfig.getoption('name')
 
-
-
-
-
-
-
-
+# def pytest_configure(config):
+#     x = config.getoption('name')
+#     print(x)  # Any logic that uses option value
 
 
